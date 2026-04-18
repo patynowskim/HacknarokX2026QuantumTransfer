005602f3a17dd9dfdc25026cab1c1d45c16c4c13
Jan Pałka <hardkanterpl@gmail.com>
2026-04-18 17:39:47 +0200

    Frontend bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Simulation from './Simulation'
 import Docs from './Docs'
-
+import { Button } from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
 
   return (
     <>
-      <Docs></Docs>
-      <Simulation></Simulation>
+    <h1>NAZWA PLACEHOLDER</h1>
+    <button className='btn btn-primary'>Docs</button>
+    <button className='btn btn-primary'>Simulation</button>
     </>
   )
 }
