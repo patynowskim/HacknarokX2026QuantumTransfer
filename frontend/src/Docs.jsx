ee5d8eeca9a85bab5fbba95483e5c469407ef307
SomeGuyNamedAdam <adaslenardt@gmail.com>
2026-04-18 19:47:10 +0200

    Docs css

--- frontend/src/Docs.jsx
+++ frontend/src/Docs.jsx
@@ -5,23 +5,36 @@ function Docs() {
 
   return (
     <>
+    <div className='navSection'>
+      <h2>Nawigacja</h2>
+      <ul>
+        <li><a href="#intro">Intro do komp kwantowych</a></li>
+        <li><a href="#protocol">Protokół QTCP</a></li>
+        <li><a href="#purpose">Zamysł projektu</a></li>
+        <li><a href="#how-it-works">Jak to działa</a></li>
+        <li><a href="#advantages">Zalety działania</a></li>
+        <li><a href="#why-it-works">Czemu to działa</a></li>
+        <li><a href="#use-cases">Możliwe użycia i edge cases</a></li>
+      </ul>
+    </div>
+    <div className='documentation'> 
       <h1>Docs</h1>
-      <section>
+      <section id="intro">
         <h3>Intro do komp kwantowych</h3>
         <p>Komputery </p>
       </section>
 
-      <section>
+      <section id="protocol">
         <h3>Protokół QTCP</h3>
         <p>Nasz projekt implementuje szyfrowanie z użyciem klucza kwantowego. Protokół ten zapewnia bezpieczeństwo komunikacji w środowisku kwantowym oraz pozwala na przejście z infrastruktury klasycznej do kwantowej.</p>
       </section>
 
-      <section>
+      <section id="purpose">
         <h3>Zamysł projektu</h3>
         <p>Serwery kwantowe do użytku komercjalnego są coraz bliższe nas. Obecne badania przewidują możliwość wdrożenia połączeń kwantowych w obecnej infrastrukturze światłowodowej, nie wpływając na istniejące połączenia internetowe. Z tego powodu protokół QTCP został zaprojektowany z myślą o integracji z istniejącą infrastrukturą. </p>
       </section>
 
-      <section>
+      <section id="how-it-works">
         <h3>Jak to działa</h3>
         <p>Protokół rozważa dwie opcje: główne - połączenie kwantowe, oraz fallback - przesył bitowy.</p>
         <div>
@@ -30,11 +43,12 @@ function Docs() {
         </div>
         <div>
           <h3>W wersji standard</h3>
-          <p></p>
+          <p>W przypadku braku możliwości ustanowienia połączenia kwantowego, protokół przechodzi na tryb standardowy, wykorzystując tworzenie klucza algorytmem ML-KEM. Ten algorytm jest odporny na ataki kwantowe, zapewniając bezpieczeństwo komunikacji nawet w przypadku braku połączenia kwantowego. 
+          </p>
         </div>
       </section>
 
-      <section>
+      <section id="advantages">
         <h3>Zalety działania</h3>
         <p></p>
         <ul>
@@ -45,7 +59,7 @@ function Docs() {
 
       
 
-      <section>
+      <section id="why-it-works">
         <h3>Czemu to działa</h3>
         <div>
           <h3>W wersji kwantowej</h3>
@@ -62,7 +76,7 @@ function Docs() {
         </div>
       </section>
 
-      <section>
+      <section id="use-cases">
         <h3>Możliwe użycia i edge cases</h3>
         <p>Dla kwant i standard:</p>
         <ol>
@@ -72,6 +86,7 @@ function Docs() {
           <li>Interferencje naturalne</li>
         </ol>
       </section>
+      </div>
     </>
   )
 }
