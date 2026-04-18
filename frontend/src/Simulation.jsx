0d673fdb028636a0f1f97968c985f17d352aa7a8
Jan Pałka <hardkanterpl@gmail.com>
2026-04-18 18:17:39 +0200

    Fronend sim

--- frontend/src/Simulation.jsx
+++ frontend/src/Simulation.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import './Simulation.css'
+import { Link } from 'react-router-dom';
 
 function Simulation() {
-
+const menuItems = [
+    { name: 'Case One', path: '/one' },
+    { name: 'Case Two', path: '/two' },
+    { name: 'Case Three', path: '/three' },
+    { name: 'Case Four', path: '/four' },
+    { name: 'Case Five', path: '/five' },
+    { name: 'Case Six', path: '/six' },
+    { name: 'Case Seven', path: '/seven' },
+    { name: 'Case Eight', path: '/eight' },
+  ];
   return (
     <>
+    <div className='w-100 bg-primary text-center container p-3 rounded text-white mt-3'>
       <h1>Simulation</h1>
+    </div>
+
     </>
   )
 }
