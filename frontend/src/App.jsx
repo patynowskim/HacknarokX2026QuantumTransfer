2ed06e2446442b6328b6c1512d751a0ddea5a207
Jan Pałka <hardkanterpl@gmail.com>
2026-04-18 18:08:06 +0200

    Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,40 +2,44 @@ import { useState } from 'react'
 import './App.css'
 import Simulation from './Simulation'
 import Docs from './Docs'
-import { Button } from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Routes, Route, Link } from 'react-router-dom';
-function App() {
 
+function Home() {
   return (
-    <>
-    <div className='container text-center w-100 '>
-      <div className="row mb-3 w-100">
+    <div className='container text-center mt-5'>
+      <div className="row mb-3">
         <div className="col-12">
-          <div className="p-3 bg-primary text-white w-100"><h1>NAZWA PLACEHOLDER</h1></div>
+          <div className="p-3 bg-primary text-white rounded">
+            <h1>NAZWA PLACEHOLDER</h1>
+          </div>
         </div>
       </div>
       <div className="row justify-content-center">
         <div className="col-auto">
           <Link to="/docs">
-          <button className='btn btn-primary p-5'>Docs</button>
+            <button className='btn btn-primary p-5'>Docs</button>
           </Link>
         </div>
         <div className="col-auto">
-          <button className='btn btn-primary p-5'>Simulation</button>
+          <Link to="/simulation">
+            <button className='btn btn-primary p-5'>Simulation</button>
+          </Link>
         </div>
       </div>
     </div>
-    <Routes>
-        <Route path="/simulation" element={<Simulation />} />
-        <Route path="/docs" element={<Docs />} />
-      </Routes>
-    </>
-
-
-
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/simulation" element={<Simulation />} />
+      <Route path="/docs" element={<Docs />} />
+    </Routes>
+  );
+}
+
+export default App;
